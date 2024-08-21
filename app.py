f257b5b1c314bc050bbede895cb61ec004a2e100
tounektimohamed <tounekti17@gmail.com>
2024-08-21 01:15:03 +0100

    Initial commit

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ CORS(app)  # Permet toutes les origines par défaut
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/convert', methods=['POST'])
 def convert_geojson():
     if 'file' not in request.files:
@@ -29,12 +30,26 @@ def convert_geojson():
         app.logger.error("Invalid GeoJSON format")
         return jsonify({"error": "Invalid GeoJSON format"}), 400
 
-    # Define projections
-    utm_proj = pyproj.CRS('EPSG:22332')
-    wgs84_proj = pyproj.CRS('EPSG:4326')
-    project = pyproj.Transformer.from_crs(utm_proj, wgs84_proj, always_xy=True).transform
+    # Vérifier si le système de coordonnées est déjà WGS 84
+    crs = geojson_data.get('crs', {}).get('properties', {}).get('name')
+    if crs == 'urn:ogc:def:crs:OGC:1.3:CRS84' or crs == 'EPSG:4326':
+        app.logger.info("Le fichier est déjà en WGS 84, pas de conversion nécessaire")
+        save_path = os.path.join('/tmp', 'converted.geojson')
+        try:
+            with open(save_path, 'w') as f:
+                json.dump(geojson_data, f, indent=4)
+        except IOError:
+            app.logger.error("Failed to save the file")
+            return jsonify({"error": "Failed to save the file"}), 500
 
+        return jsonify({"message": "Le fichier est déjà en WGS 84", "download_url": f"/download/converted.geojson"}), 200
+
+    # Si le fichier n'est pas en WGS 84, le convertir
     try:
+        utm_proj = pyproj.CRS('EPSG:22332')  # Remplacez par l'EPSG correct pour UTM
+        wgs84_proj = pyproj.CRS('EPSG:4326')
+        project = pyproj.Transformer.from_crs(utm_proj, wgs84_proj, always_xy=True).transform
+
         for feature in geojson_data.get('features', []):
             geom = shape(feature['geometry'])
             geom_wgs84 = transform(project, geom)
